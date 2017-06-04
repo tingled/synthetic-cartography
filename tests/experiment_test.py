a40ab3ffd5516eae5f70e60cc76cacde58c10f15
dt <derek.tingle@gmail.com>
2017-06-04 14:13:52 +0200

    Small refactor
    Removes base ExperimentHandler class. Adds code to
    creates new Experiment from config or loads existing

--- tests/experiment_test.py
+++ tests/experiment_test.py
@@ -7,32 +7,23 @@ from unittest import TestCase
 
 from cartography.models import Experiment
 from cartography.experiment import (
-    ExperimentHandler, SqlExperimentHandler, ParamFileLoader
+    SqlExperimentHandler, ParamFileLoader
 )
 test_db = SqliteDatabase(':memory:')
 
-class TestExperimentHandler(TestCase):
+
+class TestSqlExperimentHandler(TestCase):
     @classmethod
     def setUpClass(cls):
         fixture_dir = 'tests/fixtures/'
         cls.config_file = os.path.join(fixture_dir, 'experiment_config.yaml')
         cls.config_data = {'description': 'test experiment'}
 
-    def test_load_config(self):
-        exp = ExperimentHandler(self.config_file)
-        exp.load_config()
-        self.assertEqual(exp.config, self.config_data)
 
-    def test_iter_params(self):
-        pass
-
-
-class TestSqlExperimentHandler(TestExperimentHandler):
-    def test_create_experiment(self):
+    def test_create_from_config(self):
         with test_database(test_db, (Experiment,)):
-            exp = SqlExperimentHandler(self.config_file)
-            exp.load_config()
-            exp.create_experiment()
+            exp = SqlExperimentHandler()
+            exp.create_from_config(self.config_file)
 
             experiments = Experiment.select()
             self.assertEqual(len(experiments), 1)
@@ -42,6 +33,19 @@ class TestSqlExperimentHandler(TestExperimentHandler):
             got_dt = exp_row.datetime
             diff_dt = cur_dt - got_dt
             self.assertLess(abs(diff_dt.seconds), 3)
+
+    def test_load_existing(self):
+        with test_database(test_db, (Experiment,)):
+            expected_experiment = Experiment.create(**self.config_data)
+            expected_experiment_id = 1
+
+            exp = SqlExperimentHandler()
+            exp.load_existing(expected_experiment_id)
+            self.assertEqual(expected_experiment, exp.delegate)
+
+
+class TestSqlParamHandler(TestCase):
+    pass
 
 
 class TestParamFileLoader(TestCase):
