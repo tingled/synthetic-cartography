2fbdab83259a63767f3c7faf90db79d52d034b38
dt <derek.tingle@gmail.com>
2020-04-06 13:25:23 +0200

    WIP

--- cartography/demo.py
+++ cartography/demo.py
@@ -9,24 +9,22 @@ def rand_pitch():
     outport = open_output()
     for i in range(20):
         pitch = randint(10, 60)
-        outport.send(Message('note_on', note=pitch))
+        outport.send(Message('note_on', note=pitch, channel=1))
         sleep(0.2)
-        outport.send(Message('note_off', note=pitch))
+        outport.send(Message('note_off', note=pitch, channel=1))
 
 
 def demo_control():
     outport = open_output()
-    msgs = []
-    msgs.append(Message('note_on', note=50, time=1))
-    msgs.append(Message('note_off', note=50, time=0.5))
 
-    repeat = 10
+    chans = 16
 
-    for i, control_val in enumerate(np.linspace(0, 127, repeat)):
-        outport.send(Message('control_change', control=17, value=int(control_val)))
-        for msg in msgs:
-            outport.send(msg)
-            sleep(msg.time)
+    for chan in range(chans):
+        print(f'onset on channel {chan}')
+        outport.send(Message('note_on', note=50, time=1, channel=chan))
+        sleep(1)
+        outport.send(Message('note_off', note=50, time=0.5, channel=chan))
+        sleep(0.5)
 
 
 if __name__ == '__main__':
